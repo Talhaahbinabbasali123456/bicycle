3fe09dbcf699b5a366a96f5825ff655f2fa0e093
Talhaah Abbas <38943204+Talhaahbinabbasali123456@users.noreply.github.com>
2018-11-29 18:12:51 +0500

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,20 +1,41 @@
 function init(){
     window.onscroll = function () {scrollFunction()}
-
+    
     function scrollFunction(){
         if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50){
             document.querySelector('.nav-top').style.display = 'none';
             document.querySelector('header').style.position = 'fixed';
             document.querySelector('.nav-mid').style.padding = '10px 0px';
-
+            
         } else {
             document.querySelector('.nav-top').style.display = 'block';
             document.querySelector('header').style.position = 'static';
             document.querySelector('.nav-mid').style.padding = '35px 0px';
-
         }
     }
+    
+    var mySwiper = new Swiper ('.swiper-container', {
+        // Optional parameters
+        direction: 'horizontal',
+        loop: true,
+        
+        
+        
+        // Navigation arrows
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+        
+        autoplay: {
+            delay: 5000,
+        },
+        
+    })
 }
+
+// side nav
+
 
 function sideNavOpen() {
     document.querySelector('.side-navbar').classList.remove("hidden");
@@ -29,4 +50,60 @@ function sideNavClose() {
     document.querySelector('.side-navbar').classList.remove("visible");
     document.querySelector('.side-navbar').classList.add("hidden");
     document.querySelector('body').style.position = 'static'; 
+}
+
+
+
+// autocomplete functions
+
+$(function() {
+    $( "#autocomplete-4" ).autocomplete({
+        source: [
+            { label: "bicyle", value: "Bicycle" },
+            { label: "jumper", value: "Jumper" },
+            { label: "gear", value: "Gear" },
+            { label: "sport", value: "Sports" },
+            { label: "simple", value: "Simple" },
+            { label: "fast", value: "Fast" },
+            { label: "plane", value: "Plane" }, 
+            { label: "rider", value: "Rider" }
+        ]
+    });
+});
+
+// for small screen 
+
+$(function() {
+    $( "#autocomplete-2" ).autocomplete({
+        source: [
+            { label: "bicyle", value: "Bicycle" },
+            { label: "jumper", value: "Jumper" },
+            { label: "gear", value: "Gear" },
+            { label: "sport", value: "Sports" },
+            { label: "simple", value: "Simple" },
+            { label: "fast", value: "Fast" },
+            { label: "plane", value: "Plane" }, 
+            { label: "rider", value: "Rider" }
+        ]
+    });
+});
+
+// dropdown function
+// function dropdown() {
+//     document.querySelector('.dropdown').
+// }
+
+function dropdownOpen() {
+    document.querySelector(".dropdown").classList.remove("hidden");
+    document.querySelector(".dropdown").classList.add("visible");
+    document.querySelector(".drop-open").style.display = "none";
+    document.querySelector(".drop-close").style.display = "inline-block";
+}
+
+function dropdownClose() {
+    document.querySelector(".dropdown").classList.remove("visible");
+    document.querySelector(".dropdown").classList.add("hidden");
+    document.querySelector(".drop-open").style.display = "inline-block";
+    document.querySelector(".drop-close").style.display = "none";
+ 
 }
