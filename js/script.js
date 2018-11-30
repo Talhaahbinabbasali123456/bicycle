bbb14deb27f2143a1ee423efc7ea70b8362f851a
Talhaah Abbas <38943204+Talhaahbinabbasali123456@users.noreply.github.com>
2018-11-30 12:03:31 +0500

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ function init(){
     window.onscroll = function () {scrollFunction()}
     
     function scrollFunction(){
-        if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50){
+        if (document.body.scrollTop > 0 || document.documentElement.scrollTop > 0){
             document.querySelector('.nav-top').style.display = 'none';
             document.querySelector('header').style.position = 'fixed';
             document.querySelector('.nav-mid').style.padding = '10px 0px';
@@ -54,7 +54,7 @@ function sideNavClose() {
 
 
 
-// autocomplete functions
+// lg screen autocomplete function
 
 $(function() {
     $( "#autocomplete-4" ).autocomplete({
@@ -71,7 +71,7 @@ $(function() {
     });
 });
 
-// for small screen 
+// small screen autocomplete function
 
 $(function() {
     $( "#autocomplete-2" ).autocomplete({
@@ -88,10 +88,7 @@ $(function() {
     });
 });
 
-// dropdown function
-// function dropdown() {
-//     document.querySelector('.dropdown').
-// }
+// drop down functionality
 
 function dropdownOpen() {
     document.querySelector(".dropdown").classList.remove("hidden");
@@ -105,5 +102,67 @@ function dropdownClose() {
     document.querySelector(".dropdown").classList.add("hidden");
     document.querySelector(".drop-open").style.display = "inline-block";
     document.querySelector(".drop-close").style.display = "none";
- 
+    
 }
+
+
+// lg screen search
+
+$(document).on('keydown','#autocomplete-4',function(){
+    var searchBox = $(this);
+    var value = searchBox.val();
+    var searchIcon = $('.search-icon');
+    var crossIcon = $('.cross');
+    if(value == "")
+    {
+        crossIcon.addClass('hidden');
+        searchIcon.removeClass('hidden');
+    }
+    else
+    {
+        crossIcon.removeClass('hidden');
+        searchIcon.addClass('hidden');
+    }
+    
+});
+
+function cross() {
+    document.querySelector(".min-bar-inp").value = "";
+    document.querySelector(".cross").classList.remove("visible");
+    document.querySelector(".cross").classList.add("hidden");
+    document.querySelector(".search-icon").classList.remove("hidden");
+    document.querySelector(".search-icon").classList.add("visible");
+    
+}
+
+// small screen search
+
+$(document).on('keydown','#autocomplete-2',function(){
+    var searchBox = $(this);
+    var value = searchBox.val();
+    var searchIcon = $('.search-icon2');
+    var crossIcon = $('.cross2');
+    if(value == "")
+    {
+        crossIcon.addClass('hidden');
+        searchIcon.removeClass('hidden');
+    }
+    else
+    {
+        crossIcon.removeClass('hidden');
+        searchIcon.addClass('hidden');
+    }
+    
+});
+
+
+function crossSmallScreen() {
+    document.querySelector(".small_screen-inp").value = "";
+    document.querySelector(".cross2").classList.remove("visible");
+    document.querySelector(".cross2").classList.add("hidden");
+    document.querySelector(".search-icon2").classList.remove("hidden");
+}
+
+
+
+
